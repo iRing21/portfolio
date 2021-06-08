c4ac6eba61348d1a496fc7200342345f1477cfcd
iRing21 <ian.ring21@gmail.com>
2021-06-07 20:43:26 -0400

    Still trying to make scripts work...

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 function randomNum() {
-    return Math.floor(Math.random() * 2); // # of photos - 1
+    return Math.floor(Math.random() * 3); // # of photos +1
 }
 
 let picSelectorInput = randomNum();
 
-const pic = document.getElementById("pic");
+const pic = Document.getElementById("pic");
 
-function picSelector(picSelectorInput) {
+function picSelector() {
     switch (picSelectorInput) {
         case 0:
-            pic.innerHTML =   ;  // Lily cropped pic
+            pic.setAttribute("src", "media/lilycropped.png");  // Lily cropped pic
         case 1:
-               ; // coat cropped pic
+            pic.setAttribute("src", "media/coatcropped.png"); // coat cropped pic
         case 2: 
-               ; // me cropped pic
+            pic.setAttribute("src", "media/mecropped.png"); // me cropped pic
     }
-}
+} 
 
-console.log(picSelectorInput);
-console.log(pic.innerHTML);
+
+pic.addEventListener("onclick", picSelector); 
