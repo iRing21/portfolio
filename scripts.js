1d75c59d60b5fa16c9bce83beb2fb64f3bd179f6
iRing21 <ian.ring21@gmail.com>
2021-06-09 14:41:46 -0400

    added box hover functionality

--- scripts.js
+++ scripts.js
@@ -1,21 +1,23 @@
+
+// Beneath is for changing profile pic
+
 function randomNum() {
     return Math.floor(Math.random() * 3); // # of photos +1
 }
 
 let picSelectorInput = randomNum();
 
-const pic = Document.getElementById("pic");
+const pic = Document.getElementById('pic');
 
 function picSelector() {
     switch (picSelectorInput) {
         case 0:
-            pic.setAttribute("src", "media/lilycropped.png");  // Lily cropped pic
+            pic.setAttribute('src', 'media/lilycropped.png');  // Lily cropped pic
         case 1:
-            pic.setAttribute("src", "media/coatcropped.png"); // coat cropped pic
+            pic.setAttribute('src', 'media/coatcropped.png'); // coat cropped pic
         case 2: 
-            pic.setAttribute("src", "media/mecropped.png"); // me cropped pic
+            pic.setAttribute('src', 'media/mecropped.png'); // me cropped pic
     }
-} 
+}
 
-
-pic.addEventListener("onclick", picSelector); 
+pic.onclick = picSelector();
